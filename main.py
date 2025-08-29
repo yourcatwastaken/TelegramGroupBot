72a39d7150a5aaab898d9435cad0d2686da28f05
yourcatwastaken <127448305+yourcatwastaken@users.noreply.github.com>
2025-08-29 20:53:00 +0300

    feat: Add basic Telegram bot functionality
    This commit adds the core components of the bot:
    - basic setup with `python-telegram-bot` and `python-dotenv`.
    - command handlers for `/start` and `/help`.
    - user whitelist system.
    - handlers for unauthorized users.

---
+++ main.py
@@ -0,0 +1,76 @@
+import os
+import logging
+from telegram import Update
+from telegram.ext import Application, ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from dotenv import load_dotenv
+
+# Load the environment variables from .env file
+load_dotenv()
+
+# Configure logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+def get_allowed_users():
+    user_ids_str = os.getenv('USER_IDS')
+    if not user_ids_str:
+        logging.error('No USER_IDS found in environment variables.')
+        return []
+    allowed_users_list = []
+    try:
+        user_ids_list = user_ids_str.split(',')
+        for uid in user_ids_list:
+            integer_uid = int(uid.strip())
+            allowed_users_list.append(integer_uid)
+        return allowed_users_list
+    except ValueError:
+        logging.error("The USER_IDS variable contains non-integer values.")
+        return []
+
+ALLOWED_USERS = get_allowed_users()
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hey! I\'m the Capybara Group support bot!')
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    help_text = (
+        "Here are some commands you can use:\n"
+        "/start — starts the chat with me.\n"
+        "/help — returns the list of available commands"
+    )
+    await update.message.reply_text(help_text)
+
+async def unauthorized(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Sorry, you do not have permission to use this bot.')
+
+
+def main():
+    print('Starting bot...')
+    token = os.getenv('TELEGRAM_BOT_TOKEN')
+    if not token:
+        logging.error('No TELEGRAM_BOT_TOKEN found in environment variables.')
+        return
+
+    print(f"Allowed users list: {ALLOWED_USERS}")
+
+    app = ApplicationBuilder().token(token).build()
+
+    # Define a filter for the allowed users
+    allowed_filter = filters.User(user_id=ALLOWED_USERS)
+
+    # Commands
+    app.add_handler(CommandHandler('start', start, filters=allowed_filter))
+    app.add_handler(CommandHandler('help', help_command, filters=allowed_filter))
+
+
+    app.add_handler(MessageHandler(filters.ALL & (~allowed_filter), unauthorized))
+
+
+    print('Polling...')
+    app.run_polling(poll_interval=3)
+
+
+if __name__ == '__main__':
+    main()
